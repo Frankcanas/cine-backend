715d10715ec52fbf61cfab40347fd64d01494476
Maru-rc <ruedamanuel007@gmail.com>
2026-08-19 07:58:54 -0500

    Se paso la validacion del login de auth.routes a auth.services.ts

--- app/src/routes/auth.routes.ts
+++ app/src/routes/auth.routes.ts
@@ -1,6 +1,5 @@
-import { Router, Request, Response } from 'express';
-import User from '../models/user.model';
-
+import { Router} from 'express';
+import { loginUser } from '../services/auth.services';
 const router = Router();
 
 /**
@@ -36,37 +35,7 @@ const router = Router();
  *         description: Credenciales inválidas
  */
 
-router.post('/login', async (req: Request, res: Response) => {
-    const { email, password } = req.body;
-
-    if (!email || !password) {
-        return res.status(400).json({ message: 'Correo y contraseña son requeridos' });
-    }
-
-    try {
-        const user = await User.findOne({ where: { email: email } });
-
-        if (!user) {
-            return res.status(401).json({ message: 'Credenciales inválidas (usuario no encontrado)' });
-        }
-
-        if (user.password !== password) {
-            return res.status(401).json({ message: 'Credenciales inválidas (contraseña incorrecta)' });
-        }
-
-        return res.status(200).json({
-            message: '¡Login exitoso!',
-            datos: { 
-                id: user.id,
-                name: user.name,
-                email: user.email
-            }
-        });
-
-    } catch (error) {
-        return res.status(500).json({ message: 'Error interno del servidor', error });
-    }
-});
+router.post('/login', loginUser);
 
 export default router;
 
