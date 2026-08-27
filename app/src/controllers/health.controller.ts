518774bf63768a475c5417c35d019eb4a406c7c0
Valery <Valejo0310@gmail.com>
2026-08-26 20:26:55 -0500

    Revert "feat: implement health check endpoint and server setup"
    This reverts commit bf591c15f129847e533512d64f5f950ce73d723d.

--- app/src/controllers/health.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Request , Response } from "express";
-
-export function healthCheck (_req: Request, res: Response ){
-    res.status(200).json({
-        "status": "healthy",
-        "timestamp": "2026-06-06T12:00:00"
-    })
-}
